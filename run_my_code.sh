5ff3ab550185e6459bfa5f2db6fb9076f999dc2c
Gabriel Ford <gabe@fordltc.net>
2022-08-13 23:44:56 -0400

    Changed script path to absolute

--- run_my_code.sh
+++ run_my_code.sh
@@ -1,11 +1,11 @@
 echo "\e[1;36mI AM CREATING YOUR MAKE FILE...\nYOU ARE WELCOME...\nBEEP BOOP BEEP!\n\e[0;37m"
 
-cmake -B ./build -S ./src
+cmake -B /home/gabriel/Dev/LearningCPP/CmakeTesting/build -S ./src
 
 echo "\e[1;36mI AM MAKING YOUR EXECUTABLE...\nYOU ARE WELCOME...\nBOOP BEEP BOOP!\n\e[0;37m"
 
-make -C ./build
+make -C /home/gabriel/Dev/LearningCPP/CmakeTesting/build
 
 echo "\e[1;36mI AM RUNNING YOUR EXECUTABLE...\nYOU ARE WELCOME...\nBOOP BEEP BOOP!\n\e[0;37m"
 
-./build/main
+/home/gabriel/Dev/LearningCPP/CmakeTesting/build/main
