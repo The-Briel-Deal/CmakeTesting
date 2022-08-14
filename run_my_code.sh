9f01e66e51955eaab20ee41fbf11ce7bc7f2101c
Gabriel Ford <gabe@fordltc.net>
2022-08-13 23:33:54 -0400

    Finished shell script for running my code

--- run_my_code.sh
+++ run_my_code.sh
@@ -1,3 +1,11 @@
-echo -e "\e[1;36mI AM CREATING YOUR CMAKE FILE...\nYOU ARE WELCOME...\nBEEP BOOP BEEP!\n\e[0;37m"
+echo "\e[1;36mI AM CREATING YOUR MAKE FILE...\nYOU ARE WELCOME...\nBEEP BOOP BEEP!\n\e[0;37m"
 
 cmake -B ./build -S ./src
+
+echo "\e[1;36mI AM MAKING YOUR EXECUTABLE...\nYOU ARE WELCOME...\nBOOP BEEP BOOP!\n\e[0;37m"
+
+make -C ./build
+
+echo "\e[1;36mI AM RUNNING YOUR EXECUTABLE...\nYOU ARE WELCOME...\nBOOP BEEP BOOP!\n\e[0;37m"
+
+./build/main
